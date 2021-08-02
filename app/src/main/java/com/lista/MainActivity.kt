ba9b861a82ac75cb3eba4e57701dd8f319051bff
gabitheiss <t_gaaby@hotmail.com>
2021-08-02 10:51:27 -0300

    Lista de carros - Pronta

--- app/src/main/java/com/lista/MainActivity.kt
+++ app/src/main/java/com/lista/MainActivity.kt
@@ -2,7 +2,10 @@ package com.lista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.lista.adapter.AdapterRecyclerView
+import com.lista.model.Car
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +18,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val listOfCars = listOf<Car>(
+            Car("Chevrolet", "Celta" , 2012),
+            Car("Chevrolet", "S10" , 2020),
+            Car("Fiat", "Palio" , 2018),
+            Car("Fiat", "Strada" , 2010),
+            Car("VM", "Gol" , 2010),
+            Car("VM", "Amarok" , 2010),
+
+        )
+
         carRecyclerView = findViewById(R.id.carsRecyclerView)
+        carRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        carRecyclerView.adapter = AdapterRecyclerView(listOfCars)
 
 
 
